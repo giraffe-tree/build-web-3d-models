332fd6310b938f6f0778c4b0bb46d0540c982e6f
giraffetree <giraffetree1@gmail.com>
2026-08-02 10:58:13 +0800

    remove deprecated shadow mode

--- showcase/src/main.js
+++ showcase/src/main.js
@@ -25,7 +25,7 @@ renderer.outputColorSpace = THREE.SRGBColorSpace;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 1.08;
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.type = THREE.PCFShadowMap;
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("#090b0f");
